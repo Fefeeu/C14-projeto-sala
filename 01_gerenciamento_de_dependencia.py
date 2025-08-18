49bdaf546ebf9be070bf631898712c4500005280
Felipe Ferreira <felipefcgp.se@gmail.com>
2025-08-18 20:49:49 -0300

    feat: forcando merge conflict

--- 01_gerenciamento_de_dependencia.py
+++ 01_gerenciamento_de_dependencia.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("auxiliar/BTD6 Rounds RBE - Rounds.csv")
 
-x = df["Round"][0:100]
-y = df["Bloon RBE"][0:100]
+x = df["Round"][0:90]
+y = df["Bloon RBE"][0:90]
 
-plt.figure(dpi=400)
+plt.figure(dpi=300)
 plt.plot(x, y)
 plt.show()
