7058df4bee13b8e47202cfd596b944d9b6e0e5aa
Felipe Ferreira <felipefcgp.se@gmail.com>
2025-08-19 22:14:28 -0300

    Revert "git add 01_tabela_RBE.py ! FORCANDO EXISTIR UM ERRO git add 01_tabela_RBE.py !"
    This reverts commit db2c8c0be9b42f695ed8e517e78e5cae93aa38c8.

--- 01_tabela_RBE.py
+++ 01_tabela_RBE.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("auxiliar/BTD6 Rounds RBE - Rounds.csv")
 
 x = df["Round"][0:100]
-y = df["Bloon RBE"][0:100
+y = df["Bloon RBE"][0:100]
 
 # %%
 descricao_rbe = df["Bloon RBE"].describe()
-print(descricao_rbe
+print(descricao_rbe)
 
 # %%
-plt.figure(dpi=[])
+plt.figure(dpi=500)
 
 plt.plot(x, y)
 plt.show()
